079040c6ed77ba716ac962d97c59f196beb045e7
sharifov-aslbek <sharifovaslbek5@gmail.com>
2026-03-15 23:21:24 +0500

    errors fixed

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,22 @@
+<script setup lang="ts">
+import router from '@/router';
+
+</script>
+
 <template>
   <nav class="bg-black text-white">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="flex justify-between items-center h-16">
-        <div class="flex items-center space-x-2">
+        <router-link to="/" class="flex items-center space-x-2">
           <div class="text-2xl font-bold">⋮ MilliyMock</div>
-        </div>
+        </router-link>
 
         <div class="hidden md:flex items-center space-x-8">
           <a href="#" class="hover:text-blue-400 transition">SAT</a>
           <a href="#" class="hover:text-blue-400 transition">ACT</a>
           <a href="#" class="hover:text-blue-400 transition">PSAT</a>
           <a href="#" class="hover:text-blue-400 transition">SHSAT</a>
-          <a href="pricing" class="hover:text-blue-400 transition">Pricing</a>
+          <router-link to="/pricing" class="hover:text-blue-400 transition">Pricing</router-link>
           <div class="flex items-center space-x-1 hover:text-blue-400 transition cursor-pointer">
             <span>Resources</span>
             <span>▼</span>
