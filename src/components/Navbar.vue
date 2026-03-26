05fe47a1e24a8538753492b233533b36a15dfd39
sharifov-aslbek <sharifovaslbek5@gmail.com>
2026-03-26 23:01:42 +0500

    button updated

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -76,7 +76,7 @@ const closeMenu = () => {
           <!-- Desktop Button -->
           <router-link to="/login" class="hidden md:block">
             <button
-              class="rounded-lg bg-blue-600 px-5 py-2 text-sm font-medium text-white transition hover:bg-blue-700"
+              class="rounded-lg bg-white px-5 py-2 text-sm font-medium text-black transition hover:bg-blue-700"
             >
               {{ t('navbar.bookDemo') }}
             </button>
